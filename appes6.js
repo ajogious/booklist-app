d366ff3a6bab64cadfd479998727aa55034cd795
Ajoge Abdulmumuni Adavize <ajogious@gmail.com>
2022-09-07 16:14:35 +0100

    year added

--- appes6.js
+++ appes6.js
@@ -2,9 +2,10 @@
 
 // === Book Constructor ===
 class Book {
-  constructor(title, author, isbn) {
+  constructor(title, author, year, isbn) {
     this.title = title;
     this.author = author;
+    this.year = year;
     this.isbn = isbn;
   }
 }
@@ -19,6 +20,7 @@ class UI {
     row.innerHTML = `
     <td>${book.title}</td>
     <td>${book.author}</td>
+    <td>${book.year}</td>
     <td>${book.isbn}</td>
     <td><a href="#" class="delete">X</a></td>
   `;
@@ -35,6 +37,7 @@ class UI {
   clearField() {
     document.getElementById('title').value = '',
       document.getElementById('author').value = '',
+      document.getElementById('year').value = '',
       document.getElementById('isbn').value = '';
   }
   // === message ===
@@ -99,16 +102,17 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
   // === get form values ===
   const title = document.getElementById('title').value,
         author = document.getElementById('author').value,
+        year = document.getElementById('year').value,
         isbn = document.getElementById('isbn').value;
   
   // === Instantiate book ===
-  const book = new Book(title, author, isbn)
+  const book = new Book(title, author, year, isbn)
 
   // === instantiate UI ===
   const ui = new UI();
 
   // === validate === 
-  if (title === '' || author === '' || isbn === '') {
+  if (title === '' || author === '' || year === '' || isbn === '') {
     // === error alert ===
     ui.showAlert('Please fill in all fields', 'error')
   } else {
